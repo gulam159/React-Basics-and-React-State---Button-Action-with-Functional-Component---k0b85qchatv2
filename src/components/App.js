a4e1aae94d76b50cec388261c9f1088be371fe7c
Gulam Mohiuddeen <90964388+gulam159@users.noreply.github.com>
2022-03-26 16:04:16 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,14 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const[state, setState] = useState('');
+  const showText=()=>{
+    setState(" Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy")
+  }
   return (
     <div id="main">
-      // Do not alter the main div
+      <p id='click'>{state}</p>
+    <button id='click' onClick={showText}> Show Text </button>
     </div>
   );
 }
