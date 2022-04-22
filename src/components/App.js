1f3948763398d2ff2134c9798e9571d73fb766c7
Gulam Mohiuddeen <90964388+gulam159@users.noreply.github.com>
2022-04-23 03:19:46 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,44 +1,24 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
-//   const showText=()=>{
-//     const para = document.createElement("p");
-//     para.setAttribute('id', 'click');
-//     para.innerText = "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
-//     document.body.appendChild(para);
-//   };
-  
-  const [showPara, setShowPara] = useState("");
-   const showText = () => {
-//   	setShowPara(true)
-     setShowPara("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy")
-//      set showPara  state to negation of current value
-  }
-  
+  const [state, setState] = useState("");
+  const handleClick = () => {
+    setState(
+      <p id="para">
+        Hello, I've learnt to use the full-stack evaluation tool. This makes me
+        so happy
+      </p>
+    );
+  };
   return (
     <div id="main">
-    <button id='click' onClick={showText}> Show Text </button>
-//  {showPara && <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
-<p id='para'>{showPara}</p>
+      <button id="click" onClick={handleClick}>
+        Add
+      </button>
+      {state}
     </div>
   );
 }
 
-
 export default App;
-
-
-
-
-//   const[state, setState] = useState('');
-//   const showText=()=>{
-//     setState("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy");
-//   };
-//   return (
-//     <div id="main">
-//       <p id='click'>{state}</p>
-//     <button id='click' onClick={showText}> Show Text </button>
-//     </div>
-//   );
-  
